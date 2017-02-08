c5bf235b9f154cc91f49b22de7743567c1e05058
Joey Piccola <joey@joeypiccola.com>
2017-02-07 21:36:47 -0700

    added system memory func

---
+++ Get-SystemMemory.ps1
@@ -0,0 +1,7 @@
+﻿Function Get-SystemMemory
+{
+    $memory = $null
+    get-ciminstance -class 'cim_physicalmemory' | %{$memory += $_.capacity}
+    $memory = $memory/1024/1024
+    write-output $memory
+}
